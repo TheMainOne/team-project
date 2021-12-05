c203ecee6ad9279db0887c4588d0b25f84448c25
Maksym Lvov <leva880@ukr.net>
2021-12-05 18:19:31 +0300

    added a button to scroll to the top of the page

---
+++ src/js/back-to-top-btn.js
@@ -0,0 +1,22 @@
+export { scrollFunction, backToTop };
+let mybutton = document.querySelector(".btn-back-to-top");
+    
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "flex";
+    mybutton.style.justifyContent = "center";
+    mybutton.style.alignItems = "center";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function backToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+});
+}
