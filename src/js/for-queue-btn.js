4862868ad6b2009c7906d8710d36067c1579513a
AlexKarb <alex.workinfo97@gmail.com>
2021-12-10 11:18:18 +0200

    add second

--- src/js/for-queue-btn.js
+++ src/js/for-queue-btn.js
@@ -24,52 +24,42 @@ export function queueRemoveEventListener() {
 
 
 export async function onClickBtnQueue(e) {
-  const galleryData = refs.gallery.dataset.gallery;
+
+  const gallaryData = refs.gallery.dataset.gallery;
   const refQueueBtn = e.currentTarget;
   const movieId = Number(document.querySelector('.movie').dataset.id);
-  let ourFilm = null;
+  const filmOfWeek = await load(TRENDING.WEEK).results;
+  let ourFilm = filmOfWeek.find(film => film.id === movieId);
 
-  const filmOfWeek = await load(TRENDING.WEEK);
-  if (filmOfWeek) {
-    ourFilm = filmOfWeek.results.find(film => film.id === movieId);
-  }
-
-  const loadedSearch = load(SEARCH);
-  if (!ourFilm && loadedSearch) {
-    ourFilm = loadedSearch.results.find(film => film.id === movieId);
-  }
-
-  const loadedWatched = load(WATCHED);
-  if (!ourFilm && loadedWatched) {
-    ourFilm = loadedWatched.find(film => film.id === movieId);
-  }
-
-  const loadedQueue = load(QUEUE);
-  if (!ourFilm && loadedQueue) {
-    ourFilm = loadedQueue.find(film => film.id === movieId);
-  }
-
+   if (!ourFilm) {
+     const arrayOfFilms = load(SEARCH).results
+     ourFilm = arrayOfFilms.find(film => film.id === movieId);  
+    } 
+  
+  
+  
   if (refQueueBtn.dataset.action === 'add-to-queue') {
-    addToQueue(refQueueBtn, ourFilm);
-    return;
+        addToQueue(refQueueBtn, ourFilm);
+        return
   } else {
-    console.log('remove', { ourFilm });
-    removeFromQueue(refQueueBtn, ourFilm);
+        removeFromQueue(refQueueBtn, ourFilm);
+    }    
+  
+  
+  if (gallaryData === "queue") {
+    refsGallery.innerHTML = ''
+    renderGallery(load(QUEUE));
+    setPagination(QUEUE, load(QUEUE).length)
+  };
 
-    //
-    // const newQueue = load(QUEUE);
-    // if (newQueue) {
-    //   renderGallery(newQueue);
-    //   setPagination(QUEUE, newQueue.length);
-    // }
   }
 
-  if (galleryData === 'queue') {
-    refsGallery.innerHTML = '';
-    const loadedQueue = load(QUEUE);
-    if (loadedQueue) {
-      renderGallery(loadedQueue);
-      setPagination(QUEUE, loadedQueue.length);
-    }
-  }
-}
+//   if (galleryData === 'queue') {
+//     refsGallery.innerHTML = '';
+//     const loadedQueue = load(QUEUE);
+//     if (loadedQueue) {
+//       renderGallery(loadedQueue);
+//       setPagination(QUEUE, loadedQueue.length);
+//     }
+//   }
+// }
