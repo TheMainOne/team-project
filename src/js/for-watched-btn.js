c94fed75be30b76252d9caa0483f0d67712d0175
Dima Halaiskiy <dgalaiskiy@icloud.com>
2021-12-10 21:32:28 +0200

    check the attribute on gallery on render

--- src/js/for-watched-btn.js
+++ src/js/for-watched-btn.js
@@ -6,7 +6,9 @@ import getRefs from './refs';
 import { renderGallery } from './init-gallery';
 import { renderWatchedVideos } from './render-watched';
 import { setPagination } from './pagination';
-let dataGallery = getRefs().gallery.dataset.gallery;
+
+const refs = getRefs();
+let isWatched = refs.gallery.dataset.gallery;
 
 const { TRENDING, QUEUE, SEARCH, WATCHED } = videoapi.keys;
 
@@ -22,39 +24,35 @@ export function watchedRemoveEventListener() {
 
 export async function onClickBtnWatched(e) {
   const refWatchedBtn = e.currentTarget;
-  console.log('onClickBtnWatched ~ refWatchedBtn', refWatchedBtn);
   const movieId = Number(document.querySelector('.movie').dataset.id);
-  console.log('onClickBtnWatched ~ movieId', movieId);
 
   let filmOfWeek = await load(TRENDING.WEEK).results;
-  console.log(`trending`, filmOfWeek);
   let currentMovie = filmOfWeek.find(movie => movie.id === movieId);
-  console.log('onClickBtnWatched ~ currentMovie', currentMovie);
 
   if (!currentMovie) {
     filmOfWeek = await load(WATCHED);
     if (filmOfWeek) {
       currentMovie = filmOfWeek.find(movie => movie.id === movieId);
-      console.log('onClickBtnWatched ~ currentMovie', currentMovie);
     }
   }
 
   if (!currentMovie) {
     filmOfWeek = await load(SEARCH);
-    console.log('onClickBtnWatched ~ filmOfWeek', filmOfWeek);
 
     if (filmOfWeek) {
       currentMovie = filmOfWeek.results.find(movie => movie.id === movieId);
-      console.log(`search`, currentMovie);
     }
   }
 
+  const rerenderWatchGallery = () => {
+    isWatched === 'watch' ? renderWatchedVideos() : null;
+  };
+
   if (refWatchedBtn.dataset.action === 'add-to-watched') {
-    console.log('add', currentMovie);
-    return watched.addToWatch(refWatchedBtn, currentMovie);
+    watched.addToWatch(refWatchedBtn, currentMovie);
+    rerenderWatchGallery();
   } else {
-    console.log('remove', { currentMovie });
     watched.removeFromWatched(refWatchedBtn, currentMovie);
-    renderWatchedVideos();
+    rerenderWatchGallery();
   }
 }
